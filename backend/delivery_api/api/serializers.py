e83ff93734170a0f37b178cab5e252c5e0d56572
ahmetbicer <ahmtbicertr@gmail.com>
2020-11-07 22:34:10 +0300

    add custom login view based on obtain auth token view

--- backend/delivery_api/api/serializers.py
+++ backend/delivery_api/api/serializers.py
@@ -9,19 +9,16 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ('location',)
 class UserSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer()
     class Meta:
         model = User
-        fields = ('username','password','profile')
+        fields = ('username', 'password', 'first_name')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        profile_data = validated_data.pop('profile')
         user = User(
-            username=validated_data['username']
+            username=validated_data['username'],
+            first_name=validated_data['first_name']
         )
-        profile = Profile(user=user, location=profile_data["location"])
         user.set_password(validated_data['password'])
         user.save()
-        profile.save()
         return user
--- backend/delivery_api/api/urls.py
+++ backend/delivery_api/api/urls.py
@@ -3,7 +3,7 @@ from api import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    url(r'login', obtain_auth_token, name="login"),
+    url(r'login', views.login, name="login"),
+    url(r'register', views.register, name="register"),
     url(r'users', views.users, name="users"),
-    url(r'create-user', views.create_user, name="create-user"),
 ]
