df813bf2285873692a821d690409bbb8758882db
ahmetbicer <ahmtbicertr@gmail.com>
2020-11-07 15:06:58 +0300

    implement login screen

--- src/navigation/auth-stack-navigator.js
+++ src/navigation/auth-stack-navigator.js
@@ -8,8 +8,16 @@ const Stack = createStackNavigator();
 export default function AuthStackNavigator() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen
+                options={{ headerShown: false }}
+                name="Login"
+                component={LoginScreen}
+            />
+            <Stack.Screen
+                options={{ headerShown: false }}
+                name="Register"
+                component={RegisterScreen}
+            />
         </Stack.Navigator>
     );
 }
--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -1,16 +1,109 @@
-import React, { useContext } from 'react';
-import { View } from 'react-native';
-import { Button, Title } from 'react-native-paper';
+import React, { useContext, useState } from 'react';
+import { Keyboard, StyleSheet, ToastAndroid, View } from 'react-native';
+import { ActivityIndicator, Button, TextInput, Title } from 'react-native-paper';
 import colors from '../../constants/colors';
 import { AuthContext } from '../../providers/AuthContext';
+import LottieView from 'lottie-react-native';
 
 export default function LoginScreen(props) {
   const { login } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator color={colors.yellow} size={"large"} />
+      </View>
+    )
+  }
+
+  async function authenticate() {
+    Keyboard.dismiss();
+    setLoading(true);
+
+    let authenticated = await login(email, password);
+
+    if (!authenticated) {
+      setLoading(false);
+      ToastAndroid.showWithGravityAndOffset(
+        "Wrong Credentials",
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+        0,
+        400
+      )
+    }
+  }
 
   return (
-    <View style={{ flex: 1, backgroundColor: colors.white }}>
-      <Title>login</Title>
-      <Button onPress={login}>login</Button>
+    <View style={styles.container}>
+      <View style={styles.lottie_container}>
+        <LottieView
+          source={require('../../assets/lottie/login.json')}
+          style={styles.lottie}
+          autoPlay
+          loop
+        />
+      </View>
+      <View style={styles.login}>
+        <TextInput
+          mode={"outlined"}
+          label={"Email"}
+          value={email}
+          onChangeText={(value) => setEmail(value)}
+          theme={{ colors: { primary: colors.black } }}
+        />
+        <TextInput
+          mode={"outlined"}
+          label={"Password"}
+          value={password}
+          onChangeText={(value) => setPassword(value)}
+          secureTextEntry={true}
+          theme={{ colors: { primary: colors.black } }}
+        />
+        <Button
+          compact={true}
+          mode="contained"
+          onPress={authenticate}
+          contentStyle={{ height: 50 }}
+          style={{ marginTop: 10 }}
+          color={colors.yellow}>
+          Login
+      </Button>
+      </View>
+      <View style={styles.register}>
+        <Title style={styles.register_title}>Don't have an account?</Title>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    padding: 20,
+  },
+  lottie_container: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  lottie: {
+    width: 200,
+    height: 200,
+  },
+  login: {
+    flex: 3,
+  },
+  register: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  register_title: {
+    fontSize: 16,
+    lineHeight: 16,
+  }
+})
