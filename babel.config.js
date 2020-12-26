07dd5cf17c2007c15074cc1284538ef540d18f13
ahmetbicer <ahmtbicertr@gmail.com>
2020-12-26 13:22:40 +0300

    update reanimated and bottomsheet

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
+  plugins: [
+    'react-native-paper/babel',
+    'react-native-reanimated/plugin'
+  ]
 };
